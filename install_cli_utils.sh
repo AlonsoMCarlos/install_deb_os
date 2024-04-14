2b7733ab048d9c6362df0265e071ea9c434963cd
Carlos Alonso Martín <sr.titere@gmail.com>
2024-04-14 11:06:39 +0200

    feat: add install_cli_utils

---
+++ install_cli_utils.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Installing CLI utilities..."
+sudo apt update
+sudo apt upgrade -y
+utils=(curl wget unzip htop git fish unrar-free p7zip-full fd-find ripgrep bat lsd jq)
+sudo apt install -y "${utils[@]}"
+
+echo "Installing volta..."
+curl https://get.volta.sh | bash
+
+echo "Installing node..."
+volta install node@lts
+
+echo "Installing pnpm..."
+volta install pnpm
+
+
+echo "Installing zoxide..."
+curl -sS https://webinstall.dev/zoxide | bash
+
+echo "CLI utilities installed successfully!"
